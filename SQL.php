c880f70df22831fca29c8ed901caa0d56e1cf241
artnum <etienne@artisan-numerique.ch>
2018-08-20 13:42:36 +0200

    Copyright

--- SQL.php
+++ SQL.php
@@ -1,6 +1,6 @@
 <?PHP
 /*- 
- * Copyright (c) 2017 Etienne Bagnoud <etienne@artisan-numerique.ch>
+ * Copyright (c) 2017-2018 Etienne Bagnoud <etienne@artisan-numerique.ch>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
