2db25191a284070e6741b94f2294c0375e72a333
artnum <etienne@artisan-numerique.ch>
2019-06-07 10:28:59 +0200

    Use Op class

--- LDAP.php
+++ LDAP.php
@@ -26,241 +26,247 @@
  */
 Namespace artnum;
 
-class LDAP  {
+class LDAP extends \artnum\JStore\Op {
+  protected $DB;
+  protected $Suffix;
+  protected $Config;
+  protected $Attribute;
 
-   protected $DB;
-   protected $Suffix; 
-   protected $Config;
-   protected $Attribute;
+  function __construct($db, $suffix, $attributes, $config) {
+    $this->DB = $db;
+    $this->Suffix = $suffix;
+    $this->Config = $config;
+    $this->Binary = array();
 
-   function __construct($db, $suffix, $attributes, $config) {
-      $this->DB = $db;
-      $this->Suffix = $suffix;
-      $this->Config = $config;
-      $this->Binary = array();
-
-      if(is_array($this->Config['binary'])) {
-         foreach($this->Config['binary'] as $b) {
-            array_push($this->Binary, strtolower($b));
-         }
+    if(is_array($this->Config['binary'])) {
+      foreach($this->Config['binary'] as $b) {
+        array_push($this->Binary, strtolower($b));
       }
+    }
 
-      if(is_array($attributes)) {
-         $this->Attribute = array();
-         foreach($attributes as $attr) {
-            array_push($this->Attribute, strtolower($attr));
-         }
+    if(is_array($attributes)) {
+      $this->Attribute = array();
+      foreach($attributes as $attr) {
+        array_push($this->Attribute, strtolower($attr));
+      }
+    } else {
+      $this->Attribute = NULL;
+    }
+  }
+
+  function dbtype() {
+    return 'ldap';
+  }
+
+  function set_db($db) {
+    $this->DB = $db;
+  }
+
+  function _dn($dn = NULL) {
+    $ret = NULL;
+    if(!is_null($this->Suffix)) {
+      if(empty($dn) || is_null($dn)) {
+        $ret = $this->Suffix;
       } else {
-         $this->Attribute = NULL;
+        $ret = $dn . ',' . $this->Suffix;
       }
-   }
+    }
 
-   function dbtype() {
-      return 'ldap';
-   }
+    if ($ret != NULL) {
+      return $ret;
+    }
 
-   function set_db($db) {
-      $this->DB = $db;
-   }
+    return NULL;
+  }
 
-   function _dn($dn = NULL) {
-      $ret = NULL;
-      if(!is_null($this->Suffix)) {
-         if(empty($dn) || is_null($dn)) {
-            $ret = $this->Suffix;
-         } else {
-            $ret = $dn . ',' . $this->Suffix;
-         }
-      }
+  function get($dn) {
+    $c = $this->DB->readable();
+    $res = @ldap_read($c, $this->_dn($dn), '(objectclass=*)', $this->Attribute);
+    if($res && ldap_count_entries($c, $res) == 1) {
+      return ldap_first_entry($c, $res);
+    }
+    return NULL;
+  }
 
-      if ($ret != NULL) {
-         return $ret;
-      }
-
-      return NULL;
-   }
-
-   function get($dn) {
-      $c = $this->DB->readable();
-      $res = @ldap_read($c, $this->_dn($dn), '(objectclass=*)', $this->Attribute);
-      if($res && ldap_count_entries($c, $res) == 1) {
-         return ldap_first_entry($c, $res);
-      }
-      return NULL;
-   }
-
-   function read($dn) {
-      $c = $this->DB->readable();
-      $entry = $this->get(rawurldecode($dn));
-      if($entry) {
-         $_ber = null;
-         $dn = ldap_get_dn($c, $entry);
-         $dn = ldap_explode_dn($dn, 0);
-         $ret = array('IDent' => rawurlencode($dn[0]));
-         for($attr = ldap_first_attribute($c, $entry, $_ber); $attr !== FALSE; $attr = ldap_next_attribute($c, $entry, $_ber)) {
-            $attr = strtolower($attr); $keep = true;
-            if(! is_null($this->Attribute) && ! in_array($attr, $this->Attribute)) {
-               $keep == false;
+  function _read($dn) {
+    $c = $this->DB->readable();
+    $entry = $this->get(rawurldecode($dn));
+    if($entry) {
+      $_ber = null;
+      $dn = ldap_get_dn($c, $entry);
+      $dn = ldap_explode_dn($dn, 0);
+      $ret = array('IDent' => rawurlencode($dn[0]));
+      for($attr = ldap_first_attribute($c, $entry, $_ber); $attr !== FALSE; $attr = ldap_next_attribute($c, $entry, $_ber)) {
+        $attr = strtolower($attr); $keep = true;
+        if(! is_null($this->Attribute) && ! in_array($attr, $this->Attribute)) {
+          $keep == false;
+        }
+        if($keep) {
+          $value = FALSE;
+          if(in_array($attr, $this->Binary)) {
+            $value = ldap_get_values_len($c, $entry, $attr);
+            for($i = 0; $i < $value['count']; $i++) {
+              $value[$i] = base64_encode($value[$i]);
             }
-            if($keep) {
-               $value = FALSE;
-               if(in_array($attr, $this->Binary)) {
-                  $value = ldap_get_values_len($c, $entry, $attr);
-                  for($i = 0; $i < $value['count']; $i++) {
-                     $value[$i] = base64_encode($value[$i]);
-                  }
-               } else {
-                  $value = ldap_get_values($c, $entry, $attr);
-               }
+          } else {
+            $value = ldap_get_values($c, $entry, $attr);
+          }
 
-               if($value !== FALSE) {
-                  if($value['count'] == 1) {
-                     $value = $value[0];
-                  } else {
-                     unset($value['count']);
-                  }
-                  $ret[$attr] = $value;
-               }
-            }
-         }
-         return array(array($ret), 1);
-      }
-      return array(NULL, 0);
-   }
-
-   function delete($dn) {
-      $c = $this->DB->writable();
-      if($this->exists($dn)) {
-         return array(ldap_delete($c, $this->_dn($dn)), 0);
-      } 
-   }
-
-   function exists($dn) {
-      $c = $this->DB->readable();
-      $res = ldap_read($c, $this->_dn($dn), '(objectclass=*)', array('dn'));
-      if($res && ldap_count_entries($c, $res) == 1) {
-         return TRUE;
-      }
-      return FALSE;
-   }
-
-   function prepareSearch($searches) {
-      $op = ''; $s = 0;
-      if(! is_array($searches)) {
-         return '(objectclass=*)';
-      }
-      $filter = array();
-      foreach($searches as $name => $terms) {
-         if($name[0] == '_') { continue; }
-         if(!is_array($terms)) {
-            $terms = array($terms);
-         }
-         $f = array();
-         foreach($terms as $search) {
-            $value = substr($search, 1);
-            switch($search[0]) {
-               default:
-                  $value = $search;
-               case '=': $op = $name . '=' . trim($value); break;
-               case '~': $op = $name . '~=' . trim($value); break;
-               case '!': $op = '!(' . $name . '=' . trim($value) . ')'; break;
-               case '-': $op = '!(' . $name . '=*)'; break;
-               case '*': 
-                  $op = $name . '=*'; break;
-               case '<':
-                     if($search[1] == '=') {
-                        $value = trim(substr($value, 1));
-                        $op = $name . '<=' . $value;
-                     } else {
-                        $op = $name . '<' . trim($value); 
-                     }
-                     break;
-               case '>': 
-                     if($search[1] == '=') {
-                        $value = trim(substr($value, 1));
-                        $op = $name . '>=' . $value;
-                     } else {
-                        $op = $name . '>' . trim($value); 
-                     }
-                     break;
-            }
-            $f[] = $op; $s++;
-         }
-         if (count($f) == 1) {
-            $filter[$name] = $f[0];
-         } else if (count($f) > 1) {
-            $x = '';
-            foreach ($f as $_f) {
-               $x .= '(' . $_f . ')';
-            }
-            $f = $x;
-            if(!isset($searches['_' . $name])) {
-               $filter[$name] = '&' . $f;
+          if($value !== FALSE) {
+            if($value['count'] == 1) {
+              $value = $value[0];
             } else {
-               switch(strtolower($searches['_' . $name])) {
-                  default:
-                  case 'and':
-                     $filter[$name] = '&' . $f; break;
-                  case 'or':
-                     $filter[$name] = '|' . $f; break;
-               }
+              unset($value['count']);
             }
-         }
+            $ret[$attr] = $value;
+          }
+        }
       }
-      if (isset($searches['_rules']) && is_string($searches['_rules'])) {
-         $rules = $searches['_rules'];
-         foreach ($filter as $k => $v) {
-            $rules = str_replace('_' . $k . '_', $v, $rules);
-         }
-         $filter = $rules;
-      } else {
-         foreach ($filter as &$f) {
-            $f = '(' . $f . ')';
-         }
-         if($s == 1) {
-            /* I used "current" but had troubles with on some PHP version. This seems to do the trick */
-            foreach ($filter as $v) { $filter = $v; break; }
-         } else if($s > 1) {
-            $op = '|';
-            if (isset($searches['_operator'])) {
-               switch (strtolower($searches['_operator'])) {
-                  case 'or':
-                  default:
-                     $op = '|';
-                     break;
-                  case 'and':
-                     $op = '&';
-                     break;
-               }
-            }
-            $filter = '('. $op . implode('', $filter) . ')';
-         } else {
-            $filter = '(objectclass=*)';
-         }
-      }
-      return $filter;
-   }
+      return array(array($ret), 1);
+    }
+    return array(NULL, 0);
+  }
 
-   function listing($options) {
-      $c = $this->DB->readable();
-      if(isset($options['search'])) {
-            $filter = $this->prepareSearch($options['search']);
-      } else {
-         $filter = '(objectclass=*)';
+  function _delete($dn) {
+    $c = $this->DB->writable();
+    if($this->exists($dn)) {
+      return array(ldap_delete($c, $this->_dn($dn)), 0);
+    }
+  }
+
+  function _exists($dn) {
+    $c = $this->DB->readable();
+    $res = ldap_read($c, $this->_dn($dn), '(objectclass=*)', array('dn'));
+    if($res && ldap_count_entries($c, $res) == 1) {
+      return TRUE;
+    }
+    return FALSE;
+  }
+
+  function prepareSearch($searches) {
+    $op = ''; $s = 0;
+    if(! is_array($searches)) {
+      return '(objectclass=*)';
+    }
+    $filter = array();
+    foreach($searches as $name => $terms) {
+      if($name[0] == '_') { continue; }
+      if(!is_array($terms)) {
+        $terms = array($terms);
       }
-      $res = ldap_list($c, $this->_dn(), $filter, array('dn'));
-      $ret = array();
-      if($res) {
-         for($e = ldap_first_entry($c, $res); $e; $e = ldap_next_entry($c, $e)) {
-            $dn = ldap_get_dn($c, $e);
-            $dn = ldap_explode_dn($dn, 0);
-            $r = $this->read(rawurlencode($dn[0]));
-            if ($r[1] == 1) {
-               $ret[] = $r[0][0];
+      $f = array();
+      foreach($terms as $search) {
+        $value = substr($search, 1);
+        switch($search[0]) {
+          default:
+            $value = $search;
+          case '=': $op = $name . '=' . trim($value); break;
+          case '~': $op = $name . '~=' . trim($value); break;
+          case '!': $op = '!(' . $name . '=' . trim($value) . ')'; break;
+          case '-': $op = '!(' . $name . '=*)'; break;
+          case '*':
+            $op = $name . '=*'; break;
+          case '<':
+            if($search[1] == '=') {
+              $value = trim(substr($value, 1));
+              $op = $name . '<=' . $value;
+            } else {
+              $op = $name . '<' . trim($value);
             }
-         }
+            break;
+          case '>':
+            if($search[1] == '=') {
+              $value = trim(substr($value, 1));
+              $op = $name . '>=' . $value;
+            } else {
+              $op = $name . '>' . trim($value);
+            }
+            break;
+        }
+        $f[] = $op; $s++;
       }
-      return array($ret, count($ret));
-   }
+      if (count($f) == 1) {
+        $filter[$name] = $f[0];
+      } else if (count($f) > 1) {
+        $x = '';
+        foreach ($f as $_f) {
+          $x .= '(' . $_f . ')';
+        }
+        $f = $x;
+        if(!isset($searches['_' . $name])) {
+          $filter[$name] = '&' . $f;
+        } else {
+          switch(strtolower($searches['_' . $name])) {
+            default:
+            case 'and':
+              $filter[$name] = '&' . $f; break;
+            case 'or':
+              $filter[$name] = '|' . $f; break;
+          }
+        }
+      }
+    }
+    if (isset($searches['_rules']) && is_string($searches['_rules'])) {
+      $rules = $searches['_rules'];
+      foreach ($filter as $k => $v) {
+        $rules = str_replace('_' . $k . '_', $v, $rules);
+      }
+      $filter = $rules;
+    } else {
+      foreach ($filter as &$f) {
+        $f = '(' . $f . ')';
+      }
+      if($s == 1) {
+        /* I used "current" but had troubles with on some PHP version. This seems to do the trick */
+        foreach ($filter as $v) { $filter = $v; break; }
+      } else if($s > 1) {
+        $op = '|';
+        if (isset($searches['_operator'])) {
+          switch (strtolower($searches['_operator'])) {
+            case 'or':
+            default:
+              $op = '|';
+              break;
+            case 'and':
+              $op = '&';
+              break;
+          }
+        }
+        $filter = '('. $op . implode('', $filter) . ')';
+      } else {
+        $filter = '(objectclass=*)';
+      }
+    }
+    return $filter;
+  }
+
+  function listing($options) {
+    $c = $this->DB->readable();
+    if(isset($options['search'])) {
+      $filter = $this->prepareSearch($options['search']);
+    } else {
+      $filter = '(objectclass=*)';
+    }
+    $res = ldap_list($c, $this->_dn(), $filter, array('dn'));
+    $ret = array();
+    if($res) {
+      for($e = ldap_first_entry($c, $res); $e; $e = ldap_next_entry($c, $e)) {
+        $dn = ldap_get_dn($c, $e);
+        $dn = ldap_explode_dn($dn, 0);
+        $r = $this->read(rawurlencode($dn[0]));
+        if ($r[1] == 1) {
+          $ret[] = $r[0][0];
+        }
+      }
+    }
+    return array($ret, count($ret));
+  }
+
+  function _write ($data) {
+    return null;
+  }
+  function _overwrite ($data) {
+    return null;
+  }
 }
 ?>
